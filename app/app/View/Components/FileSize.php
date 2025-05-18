7a0803426f858de6cc233cd0367785fc6e073bdc
root <root@stick231>
2025-05-18 20:55:28 +0300

    rename and small change

--- app/app/View/Components/FileSize.php
+++ app/app/View/Components/FileSize.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class fileSize extends Component
+class FileSize extends Component
 {
     public $fileSize;
     public $checkBlock;
